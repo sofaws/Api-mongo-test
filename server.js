33a38c7ad82910f2103602141fb03c4fb30b2f8a
maxime blanc <maximeblanc.dev@gmail.com>
2018-07-04 14:07:31 +0200

    update cors middleware

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(fileUpload());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 });
